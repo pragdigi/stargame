cba12e43e46f6cb5df1991c7871b6ba8dc4bf0e5
Krste Kovaceski <krste@pragdigi.com>
2019-08-18 17:00:40 +0200

    remove repeat div and buttons using map

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ### Star Match Game
+interactive kids game where you can click on the number or sum of stars displayed before timer runs out.
 
 ## Thanks to
 JSDrops for example, and create-react-app repo
--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 // Color Theme
 const colors = {
-  available: 'lightgray',
-  used: 'lightgreen',
-  wrong: 'lightcoral',
-  candidate: 'deepskyblue',
+  available: "lightgray",
+  used: "lightgreen",
+  wrong: "lightcoral",
+  candidate: "deepskyblue"
 };
 
 // Math science
@@ -36,41 +36,31 @@ const utils = {
       }
     }
     return sums[utils.random(0, sums.length)];
-  },
+  }
 };
 
 function App() {
+  const stars = 5;
   return (
-  <div className="game">
-  <div className="help">
-    Pick 1 or more numbers that sum to the number of stars
-  </div>
-  <div className="body">
-    <div className="left">
-      <div className="star" />
-      <div className="star" />
-      <div className="star" />
-      <div className="star" />
-      <div className="star" />
-      <div className="star" />
-      <div className="star" />
-      <div className="star" />
-      <div className="star" />
+    <div className="game">
+      <div className="help">
+        Pick 1 or more numbers that sum to the number of stars
+      </div>
+      <div className="body">
+        <div className="left">
+          {utils.range(1, stars).map(starId => (
+            <div key={starId} className="star" />
+          ))}
+        </div>
+        <div className="right">
+          {utils.range(1, 9).map(number =>
+            (
+              <button key={number} className="number">{number}</button>
+            ))}
+        </div>
+      </div>
+      <div className="timer">Time Remaining: 10</div>
     </div>
-    <div className="right">
-      <button className="number">1</button>
-      <button className="number">2</button>
-      <button className="number">3</button>
-      <button className="number">4</button>
-      <button className="number">5</button>
-      <button className="number">6</button>
-      <button className="number">7</button>
-      <button className="number">8</button>
-      <button className="number">9</button>
-    </div>
-  </div>
-  <div className="timer">Time Remaining: 10</div>
-</div>
   );
 }
 
